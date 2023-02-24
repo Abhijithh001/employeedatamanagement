a43077c12cea3d430b088ed91604a8bd295f5ac5
Abhijithh001 <100572360+Abhijithh001@users.noreply.github.com>
2023-02-24 11:12:19 +0530

    Add files via upload

---
+++ AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniProject.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
